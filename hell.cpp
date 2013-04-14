569ada5701acbf220a9a42cfdd57297ac31b20c1
Arihant Kumar Jain <arihant@arihant.(none)>
2013-04-14 22:29:49 +0530

    almost done

--- hell.cpp
+++ hell.cpp
@@ -1,23 +1,29 @@
 /*
-code to scramble given image to number of tiles given by user
-To run the code : g++ -o hello hello.cpp `pkg-config opencv --cflags --libs`
-		  ./hello <imagefile> <level(1 or 2 or 3 ....)>
-*/
+	JIGSAW PUZZLE
+		
+   This code is written in c++ including <cv.h> and <highgui.h> libraries 
+   to develop a jigsaw puzzle game.
+
+   An image is loaded randomly from set of images and it is cut into tiles provided 
+   by the user and stored in array. The tiles are then scrambled and displayed on right. 
+   Player need to drag all tiles and drop to right side in correct positions to complete 
+   the image. 	
+   
+   To compile the code: g++ -o hello hello.cpp `pkg-config opencv --cflags --libs`
+   To run the code: ./hello 
+ */
 
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-#include <sstream>
 #include <cv.h>						// opencv libraries
 #include <highgui.h>
 
 
 int **initial,**final;
-bool dragging = false;
 int level=0;
-int current_drag[4] = {-1};	//l->r:0 r->l:1 l->l:2 r->r:3 else -1
+int current_drag[4];
 IplImage *quarter[100];
 IplImage *white;
 IplImage *win;
@@ -30,21 +36,23 @@ CvFont font;
 
 void changedisplay(int x, int y);
 void initial_drag(int x,int y);
+
+
 // Implement mouse callback
 void my_mouse_callback( int event, int x, int y, int flags, void* param ){
 
-        switch( event ){
+	switch( event ){
 
-                case CV_EVENT_LBUTTONDOWN:
-                        initial_drag(x,y);
-                        break;
+		case CV_EVENT_LBUTTONDOWN:
+			initial_drag(x,y);
+			break;
 
-                case CV_EVENT_LBUTTONUP:
-			
-                        changedisplay(x,y);
+		case CV_EVENT_LBUTTONUP:
 
-                        break;
-        }
+			changedisplay(x,y);
+
+			break;
+	}
 }
 
 
@@ -52,29 +60,29 @@ void initial_drag(int x,int y)
 {
 
 
-current_drag[0] = -1;
-current_drag[1] = -1;
-current_drag[2] = -1;
-current_drag[3] = -1;
+	current_drag[0] = -1;
+	current_drag[1] = -1;
+	current_drag[2] = -1;
+	current_drag[3] = -1;
 
-for(int i=0;i<level*level;i++)
+	for(int i=0;i<level*level;i++)
 	{
-	        //printf("0:%d 1:%d 2:%d 3:%d x=%d y=%d\n",initial[i][1],initial[i][2],initial[i][3],initial[i][4],x,y);
+		//printf("0:%d 1:%d 2:%d 3:%d x=%d y=%d\n",initial[i][1],initial[i][2],initial[i][3],initial[i][4],x,y);
 
-   	if(initial[i][1]<=x && initial[i][3]>=x && initial[i][2]<=y && initial[i][4]>=y  && initial[i][0]!=-1)
+		if(initial[i][1]<=x && initial[i][3]>=x && initial[i][2]<=y && initial[i][4]>=y  && initial[i][0]!=-1)
 		{ current_drag[0] = i ;
-	          current_drag[2] = i ; 
-				  	
-		break; }
+			current_drag[2] = i ; 
 
-	if(final[i][1]<=x && final[i][3]>=x && final[i][2]<=y && final[i][4]>=y && final[i][0]!=-1)
-	{
-		 current_drag[1] = i ;
-                  current_drag[3] = i ;
+			break; }
 
-                break;
-	}	
-}
+		if(final[i][1]<=x && final[i][3]>=x && final[i][2]<=y && final[i][4]>=y && final[i][0]!=-1)
+		{
+			current_drag[1] = i ;
+			current_drag[3] = i ;
+
+			break;
+		}	
+	}
 }
 
 
@@ -82,151 +90,151 @@ for(int i=0;i<level*level;i++)
 void changedisplay(int x, int y)
 {
 
-int flag=1;
+	int flag=1;
 
-if(current_drag[0] == -1 && current_drag[1] == -1)
- 	return;
+	if(current_drag[0] == -1 && current_drag[1] == -1)
+		return;
 
-else
-{
-	for(int i=0;i<level*level;i++)
-        {
+	else
+	{
+		for(int i=0;i<level*level;i++)
+		{
 
-        if(final[i][1]<=x && final[i][3]>=x && final[i][2]<=y && final[i][4]>=y  )
-                {
-		
-		if(final[i][0]==-1 && current_drag[1]==-1){
-		flag=1;
-		final[i][0]=initial[current_drag[0]][0];
-		initial[current_drag[0]][0]=-1;
-		 cvSetImageROI(jumbled, cvRect(final[i][1], final[i][2], final[i][3]-final[i][1],final[i][4]-final[i][2]));
-
-                cvResize(quarter[final[i][0]], jumbled);
-		     cvResetImageROI(jumbled);
-
-		
-		for(int j=0;j<level*level;j++)
+			if(final[i][1]<=x && final[i][3]>=x && final[i][2]<=y && final[i][4]>=y  )
 			{
-			if(final[j][0]!=j)
-				{flag=0;break;}
-			
+
+				if(final[i][0]==-1 && current_drag[1]==-1){
+					flag=1;
+					final[i][0]=initial[current_drag[0]][0];
+					initial[current_drag[0]][0]=-1;
+					cvSetImageROI(jumbled, cvRect(final[i][1], final[i][2], final[i][3]-final[i][1],final[i][4]-final[i][2]));
+
+					cvResize(quarter[final[i][0]], jumbled);
+					cvResetImageROI(jumbled);
+
+
+					for(int j=0;j<level*level;j++)
+					{
+						if(final[j][0]!=j)
+						{flag=0;break;}
+
+					}
+
+					if(flag == 0){
+						cvSetImageROI(jumbled, cvRect(initial[current_drag[0]][1],initial[current_drag[0]][2],initial[current_drag[0]][3]-initial[current_drag[0]][1],initial[current_drag[0]][4]-initial[current_drag[0]][2]));
+
+						cvResize(white, jumbled);
+
+						cvResetImageROI(jumbled);
+
+						cvShowImage("mainWin", jumbled);
+					}
+
+					else if(flag==1)
+					{	over = 1;
+
+
+						cvSetImageROI(jumbled, cvRect(initial[0][1],initial[0][2],initial[level*level-1][3]-initial[0][1],initial[level*level-1][4]-initial[0][2]));
+
+						cvResize(win, jumbled);
+
+						cvResetImageROI(jumbled);
+
+						cvShowImage("mainWin", jumbled);	
+						/*			cv::addText( jumbled,"You Win", cv::Point(500,550), font);
+
+									cvResetImageROI(jumbled);
+
+									cvShowImage("mainWin", jumbled);	
+						 */			//cvReleaseImage(&jumbled);
+
+
+
+					}
+					steps++;
+				}
+
+
+				if(final[i][0]==-1 && current_drag[0]==-1){
+
+					final[i][0]=final[current_drag[3]][0];
+					final[current_drag[3]][0]=-1;
+					cvSetImageROI(jumbled, cvRect(final[i][1], final[i][2], final[i][3]-final[i][1],final[i][4]-final[i][2]));
+
+					cvResize(quarter[final[i][0]], jumbled);
+					cvResetImageROI(jumbled);
+
+					cvSetImageROI(jumbled, cvRect(final[current_drag[3]][1],final[current_drag[3]][2],final[current_drag[3]][3]-final[current_drag[3]][1],final[current_drag[3]][4]-final[current_drag[3]][2]));
+
+					cvResize(white, jumbled);
+
+					cvResetImageROI(jumbled);
+
+					cvShowImage("mainWin", jumbled);
+					steps++;
+				}
+
 			}
 
-		if(flag == 0){
-		cvSetImageROI(jumbled, cvRect(initial[current_drag[0]][1],initial[current_drag[0]][2],initial[current_drag[0]][3]-initial[current_drag[0]][1],initial[current_drag[0]][4]-initial[current_drag[0]][2]));
 
-                cvResize(white, jumbled);
 
-		     cvResetImageROI(jumbled);
-		
- 		  cvShowImage("mainWin", jumbled);
+
+			if(initial[i][1]<=x && initial[i][3]>=x && initial[i][2]<=y && initial[i][4]>=y  )
+			{
+
+				if(initial[i][0]==-1 && current_drag[1]==-1){
+
+					initial[i][0]=initial[current_drag[2]][0];
+
+					initial[current_drag[2]][0]=-1;
+
+					cvSetImageROI(jumbled, cvRect(initial[i][1], initial[i][2], initial[i][3]-initial[i][1],initial[i][4]-initial[i][2]));
+
+					cvResize(quarter[initial[i][0]], jumbled);
+					cvResetImageROI(jumbled);
+
+					cvSetImageROI(jumbled, cvRect(initial[current_drag[2]][1],initial[current_drag[2]][2],initial[current_drag[2]][3]-initial[current_drag[2]][1],initial[current_drag[2]][4]-initial[current_drag[2]][2]));
+
+					cvResize(white, jumbled);
+
+
+					cvResetImageROI(jumbled);
+
+					cvShowImage("mainWin", jumbled);
+					steps++;
+
+				}
+
+
+				if(initial[i][0]==-1 && current_drag[0]==-1){
+
+					initial[i][0]=final[current_drag[1]][0];
+
+					final[current_drag[1]][0]=-1;
+
+					cvSetImageROI(jumbled, cvRect(initial[i][1], initial[i][2], initial[i][3]-initial[i][1],initial[i][4]-initial[i][2]));
+
+					cvResize(quarter[initial[i][0]], jumbled);
+					cvResetImageROI(jumbled);
+
+					cvSetImageROI(jumbled, cvRect(final[current_drag[1]][1],final[current_drag[1]][2],final[current_drag[1]][3]-final[current_drag[1]][1],final[current_drag[1]][4]-final[current_drag[1]][2]));
+
+					cvResize(white, jumbled);
+
+
+
+					cvResetImageROI(jumbled);
+
+					cvShowImage("mainWin", jumbled);
+					steps++;    
+				}
+
+
+
+			}
+
 		}
-
-		else if(flag==1)
-		{	over = 1;
-
-
-			cvSetImageROI(jumbled, cvRect(initial[0][1],initial[0][2],initial[level*level-1][3]-initial[0][1],initial[level*level-1][4]-initial[0][2]));
-
-                cvResize(win, jumbled);
-
-		     cvResetImageROI(jumbled);
-		
- 		  cvShowImage("mainWin", jumbled);	
-/*			cv::addText( jumbled,"You Win", cv::Point(500,550), font);
-			
-			cvResetImageROI(jumbled);
-			
- 		  cvShowImage("mainWin", jumbled);	
-*/			//cvReleaseImage(&jumbled);
-			
-			
-
-		}
-		steps++;
-              }
-
-		
-		 if(final[i][0]==-1 && current_drag[0]==-1){
-                
-                final[i][0]=final[current_drag[3]][0];
-                final[current_drag[3]][0]=-1;
-                 cvSetImageROI(jumbled, cvRect(final[i][1], final[i][2], final[i][3]-final[i][1],final[i][4]-final[i][2]));
-
-                cvResize(quarter[final[i][0]], jumbled);
-                     cvResetImageROI(jumbled);
-
-                cvSetImageROI(jumbled, cvRect(final[current_drag[3]][1],final[current_drag[3]][2],final[current_drag[3]][3]-final[current_drag[3]][1],final[current_drag[3]][4]-final[current_drag[3]][2]));
-
-                cvResize(white, jumbled);
-		
-                     cvResetImageROI(jumbled);
-		
-                  cvShowImage("mainWin", jumbled);
-		steps++;
-		}
-
-	     }
-
-
-
-
-		  if(initial[i][1]<=x && initial[i][3]>=x && initial[i][2]<=y && initial[i][4]>=y  )
-                {
-
-                if(initial[i][0]==-1 && current_drag[1]==-1){
-       		
-		initial[i][0]=initial[current_drag[2]][0];
-
-                 initial[current_drag[2]][0]=-1;
-
-                 cvSetImageROI(jumbled, cvRect(initial[i][1], initial[i][2], initial[i][3]-initial[i][1],initial[i][4]-initial[i][2]));
-
-                cvResize(quarter[initial[i][0]], jumbled);
-                     cvResetImageROI(jumbled);
-
-                cvSetImageROI(jumbled, cvRect(initial[current_drag[2]][1],initial[current_drag[2]][2],initial[current_drag[2]][3]-initial[current_drag[2]][1],initial[current_drag[2]][4]-initial[current_drag[2]][2]));
-
-                cvResize(white, jumbled);
-		
-
-                     cvResetImageROI(jumbled);
-		
-                  cvShowImage("mainWin", jumbled);
-		steps++;
-		
-              }
-
-
-                if(initial[i][0]==-1 && current_drag[0]==-1){
-		
-                initial[i][0]=final[current_drag[1]][0];
-
-                 final[current_drag[1]][0]=-1;
-
-                 cvSetImageROI(jumbled, cvRect(initial[i][1], initial[i][2], initial[i][3]-initial[i][1],initial[i][4]-initial[i][2]));
-
-                cvResize(quarter[initial[i][0]], jumbled);
-                     cvResetImageROI(jumbled);
-
-                cvSetImageROI(jumbled, cvRect(final[current_drag[1]][1],final[current_drag[1]][2],final[current_drag[1]][3]-final[current_drag[1]][1],final[current_drag[1]][4]-final[current_drag[1]][2]));
-
-                cvResize(white, jumbled);
-		
-
-		
-                     cvResetImageROI(jumbled);
-		
-                  cvShowImage("mainWin", jumbled);
-		   steps++;    
-	       }
-
-
-                
-             }
-
-        }
-}
+	}
 
 }
 // function to scale image  
@@ -240,22 +248,22 @@ IplImage* img_resize(IplImage* src_img, int new_width,int new_height)
 
 void putvalue(int x, int i,int var,int x1, int y1, int x2, int y2)
 {
-if(x==0)
-{ initial[i][0]=var;
- initial[i][1]=x1;
- initial[i][2]=y1;
- initial[i][3]=x2;
- initial[i][4]=y2;
-}
+	if(x==0)
+	{ initial[i][0]=var;
+		initial[i][1]=x1;
+		initial[i][2]=y1;
+		initial[i][3]=x2;
+		initial[i][4]=y2;
+	}
 
-else
-{
- final[i][0]=var;
-final[i][1]=x1;
- final[i][2]=y1;
- final[i][3]=x2;
- final[i][4]=y2;
-}
+	else
+	{
+		final[i][0]=var;
+		final[i][1]=x1;
+		final[i][2]=y1;
+		final[i][3]=x2;
+		final[i][4]=y2;
+	}
 }
 
 int main()
@@ -274,10 +282,10 @@ int main()
 	printf("Enter the level:\n1 for 2x2 puzzle\n2 for 3x3 puzzle\n.\n.\nmaximum level allowed is 9\n");
 	scanf("%d",&level);
 
-	
+
 
 	if(level>9 || level<0)
-		{
+	{
 		printf("Level not allowed\n");
 		exit(0);
 	}
@@ -286,13 +294,13 @@ int main()
 
 	srand(time(NULL));
 
-	
+
 
 	int randimage = rand()%totalimg;	
 	// load an image  
 	img=cvLoadImage(images[randimage]);
 	img=img_resize(img,500,500);
-	
+
 	level+=1;
 
 	initial = (int **) malloc(sizeof(int *)*(level*level));
@@ -300,8 +308,8 @@ int main()
 
 	for(int i=0;i<level*level;i++)
 	{
-	initial[i] = (int *) malloc(sizeof(int)*5);
-	final[i] = (int *) malloc(sizeof(int)*5);
+		initial[i] = (int *) malloc(sizeof(int)*5);
+		final[i] = (int *) malloc(sizeof(int)*5);
 
 	}
 	// get the image data
@@ -310,7 +318,7 @@ int main()
 	step      = img->widthStep;
 	channels  = img->nChannels;
 	data      = (uchar *)img->imageData;
-	printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
+
 
 	//IplImage **quarter = (IplImage *)malloc(sizeof(IplImage *)*(level*level));
 
@@ -323,7 +331,7 @@ int main()
 	int neww=600/(level+1);
 	int newh=600/(level+1);
 
-   	//int initial[25][5],final[25][5];
+	//int initial[25][5],final[25][5];
 
 	white = img_resize(white,neww,newh);
 
@@ -341,11 +349,11 @@ int main()
 
 		if((i+1)%level == 0)
 			y=y+hl; 
-//                printf("%d %d %d %d\n",initial[i][1],initial[i][2],initial[i][3],initial[i][4]);
+		//                printf("%d %d %d %d\n",initial[i][1],initial[i][2],initial[i][3],initial[i][4]);
 
 	}
 
-	
+
 
 
 	jumbled = cvCreateImage(cvSize(1200, height+100),img->depth,img->nChannels);		//defining
@@ -355,7 +363,7 @@ int main()
 	int size = quarter[0]->width;
 	int m,n,q,gap=50;
 
-        // scrambling image tiles on left side of window 	
+	// scrambling image tiles on left side of window 	
 	for(i=0,m=space,n=space;i<level*level;i++,m+=(space+size))
 	{
 		if (i%w==0 && m!=space)
@@ -377,7 +385,7 @@ int main()
 		}
 		putvalue(0,i,q,m+gap,n+gap,m+wid+gap,n+hei+gap);
 		cvResize(quarter[q], jumbled);
-//		printf("i:%d\n",jumblearray[i]);
+		//		printf("i:%d\n",jumblearray[i]);
 		cvResetImageROI(jumbled);
 	}
 
@@ -399,10 +407,10 @@ int main()
 
 	}
 
-	
+
 	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
 	cvMoveWindow("mainWin", 100, 100);
-	
+
 	cvSetMouseCallback("mainWin", my_mouse_callback, (void*) jumbled);
 
 	font = cv::fontQt("Times",15,255,255,255,255);
@@ -412,31 +420,31 @@ int main()
 	cvShowImage("mainWin", img);
 	cvWaitKey(0);
 	cvReleaseImage(&img);
-	
+
 	// Set up the callback
 
 	// create a window
 	CvFont font = cv::fontQt("Times",15,CV_RGB(255,255,255));
 	cv::addText(jumbled,"Drag drop to move pictures", cv::Point(700,40), font);
-	
+
 	while(1)
 	{
-	//     cvResetImageROI(jumbled);
+		//     cvResetImageROI(jumbled);
 
-	cv::displayOverlay("mainWin", "JIG-SAW PUZZLE", 0);
-	
+		cv::displayOverlay("mainWin", "JIG-SAW PUZZLE", 0);
 
-	cvShowImage("mainWin", jumbled);
-	
-	if(over == 1)
-	{
-	printf("Number of steps taken: %d\n",steps);
-	exit(0);
-	}
-	cvWaitKey(0);
-	
-	//cvReleaseImage(&jumbled);
-	
+
+		cvShowImage("mainWin", jumbled);
+
+		if(over == 1)
+		{
+			printf("Number of steps taken: %d\n",steps);
+			exit(0);
+		}
+		cvWaitKey(0);
+
+		//cvReleaseImage(&jumbled);
+
 	}
 	return 0;
 }
